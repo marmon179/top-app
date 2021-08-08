797d53f1c4873e256de993be55cd422a3a04abb2
marmon179 <marmon179@gmail.com>
2021-08-08 09:57:27 +0300

    7.01

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import "../styles/globals.css";
+import type {AppProps} from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps):JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return <>
+        <Head>
+            <title>MyTop - наш лучший топ</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+                  rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />;
+    </>;
+
+
 }
+
 export default MyApp;
